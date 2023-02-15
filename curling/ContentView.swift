639f86ef58c8d49ddca8ce22589dba544389f1d9
Muto Sota <hasu_m.s@icloud.com>
2023-02-15 16:24:13 +0900

    Initial Commit

---
+++ curling/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  curling
+//
+//  Created by 武藤 颯汰 on 2023/02/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ curling/curlingApp.swift
@@ -0,0 +1,17 @@
+//
+//  curlingApp.swift
+//  curling
+//
+//  Created by 武藤 颯汰 on 2023/02/15.
+//
+
+import SwiftUI
+
+@main
+struct curlingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
